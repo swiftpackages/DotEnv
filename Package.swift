5884900f19c8de29a708b0aab978355bd4c95f49
Marz Rover <marzvrover@github.com>
2022-04-21 23:15:19 -0400

    fix swift version issues

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
             targets: ["DotEnv"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.29.0"),
+        .package(url: "https://github.com/apple/swift-nio.git", "2.23.0"..."2.39.0"),
     ],
     targets: [
         .target(
